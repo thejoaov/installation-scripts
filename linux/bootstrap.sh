49038ddeaf3664bb83e3ceef8a8628c9c866db13
João Victor <jvictorsantos852@gmail.com>
2022-02-17 05:05:51 -0300

    Update bootstrap.sh

--- linux/bootstrap.sh
+++ linux/bootstrap.sh
@@ -1,6 +1,6 @@
 sudo apt update &&\
 sudo apt install build-essential git curl wget python3 python3-pip -y &&\
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" &&\
+echo | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" &&\
 echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> ~/.profile &&\
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)" &&\
 brew install gh &&\
