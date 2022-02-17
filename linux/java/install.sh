19a8757ee83bed39f111fc378db88515676bf2ab
João Victor dos Santos Mendes <jvictorsantos852@gmail.com>
2022-02-17 04:48:56 -0300

    fix(linux): supress java installation

--- linux/java/install.sh
+++ linux/java/install.sh
@@ -1,10 +1,7 @@
 if java -version 2>&1 >/dev/null | grep -q "java version" ; then
   echo "   Java is already installed"
 else
-  echo '   Installing java openjdk 8 jdk'
-  sudo apt-get install openjdk-8-jdk
-
-  echo '   Installing java openjdk 8 jre'
-  sudo apt-get install openjdk-8-jre
+  echo '   Installing java openjdk 8 jdk and jre'
+  sudo apt-get -qq install openjdk-8-jdk openjdk-8-jre -y
 fi
 echo "✅ Success"
