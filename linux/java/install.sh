af167066d983f718ed7cffc8f86fa0138eecc2a2
João Victor <jvictorsantos852@gmail.com>
2022-02-17 05:47:01 -0300

    Update install.sh

--- linux/java/install.sh
+++ linux/java/install.sh
@@ -2,7 +2,6 @@ if java -version 2>&1 >/dev/null | grep -q "java version" ; then
   echo "   Java is already installed"
 else
   echo '   Installing java openjdk 8 jdk and jre'
-  sudo apt-get -qq install openjdk-8-jdk -y &&\
-  sudo apt-get -qq install openjdk-8-jre -y
+  sudo apt-get install -qq -o=Dpkg::Use-Pty=0 openjdk-8-jdk openjdk-8-jre -y
 fi
 echo "✅ Success"
