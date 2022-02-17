9e6281d2d68c8b138776bab813039031b9e08fb2
João Victor <jvictorsantos852@gmail.com>
2022-02-17 04:23:19 -0300

    Update install.sh

--- linux/install.sh
+++ linux/install.sh
@@ -1,6 +1,16 @@
 export BASE_URL=https://raw.githubusercontent.com/thejoaov/installation-scripts/master/linux
 
-echo "1. Installing git"
+sudo apt update &&\
+sudo apt install build-essential git curl wget python3 python3-pip -y &&\
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" &&\
+echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> ~/.profile &&\
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)" &&\
+brew install gh &&\
+gh auth login -h github.com -s user,repo,admin:public_key -w &&\
+cd $HOME && gh repo clone thejoaov/installation-scripts &&\
+cd installation-scripts/linux && ./install.sh
+echo "1. Installing git" &&\
+
 cd ./git && ./install.sh || echo "Error installing git" && cd ..
 
 echo "4. Installing java"
