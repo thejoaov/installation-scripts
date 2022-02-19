26cc0cdaa5f2d834fd192e31525d2ceaa047823d
João Victor <jvictorsantos852@gmail.com>
2022-02-19 17:20:06 -0300

    fix(linux): nvm install

--- linux/nvm/install.sh
+++ linux/nvm/install.sh
@@ -3,7 +3,8 @@ export BASE_URL=https://raw.githubusercontent.com/thejoaov/installation-scripts/
 echo "Installing NVM"
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | zsh
 
-export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 nvm install --lts
