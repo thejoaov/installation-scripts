36fd045332dff07c37fd1f67adc3533bcdafea39
João Victor <jvictorsantos852@gmail.com>
2022-02-19 17:28:18 -0300

    Update install.sh

--- linux/java/install.sh
+++ linux/java/install.sh
@@ -2,7 +2,7 @@ if java -version 2>&1 >/dev/null | grep -q "java version" ; then
   echo "   Java is already installed"
 else
   echo '   Installing java openjdk 11 jdk and jre'
-  sudo apt-get install -qq -o=Dpkg::Use-Pty=0 openjdk-11-jdk openjdk-11-jre -y
+  sudo apt-get install -qq -o=Dpkg::Use-Pty=0 openjdk-11-jdk openjdk-11-jre -y &>/dev/null
 fi
 echo "✅ Success"
 echo "java" >> $HOME/.installinfo
