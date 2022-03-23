1402189c18209cf0861d8d1468f18b8a6fe213e0
João Victor <jvictorsantos852@gmail.com>
2022-03-23 10:24:58 -0300

    Update install.sh

--- mac/install.sh
+++ mac/install.sh
@@ -1,5 +1,5 @@
 if [ 'uname -m' == "arm64" ]; then
     echo "arm64 device"
 else
-    sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/thejoaov/installation-scripts/master/mac/x86_64/install.sh)"
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/thejoaov/installation-scripts/master/mac/x86_64/install.sh)"
 fi
