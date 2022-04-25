03aa530299cf0ab52c6312e9513369a9731dc082
João Victor <jvictorsantos852@gmail.com>
2022-04-25 04:06:54 -0300

    Update install.sh

--- linux/android/install.sh
+++ linux/android/install.sh
@@ -1,32 +1,35 @@
 export BASE_URL=https://raw.githubusercontent.com/thejoaov/installation-scripts/master
 
-mkdir ./Android
-mkdir ./Android/Sdk
+# mkdir ./Android
+# mkdir ./Android/Sdk
 
-if [[ -f "$HOME/Android/Sdk" ]]; then
-  sudo apt-get install lib32z1 unzip -y 
-  wget https://dl.google.com/android/repository/sdk-tools-linux-4333796.zip --quiet -P $HOME
-  unzip $HOME/sdk-tools-linux-4333796.zip -d ./Android/Sdk
-  rm $HOME/sdk-tools-linux-4333796.zip
+mkdir $HOME/Android
+mkdir $HOME/Android/Sdk
 
-  cd $HOME/Android/Sdk/tools/bin
-  source $HOME/.bashrc
-  export ANDROID_HOME=$HOME/Android/Sdk
-  export PATH=$PATH:$ANDROID_HOME/tools
-  export PATH=$PATH:$ANDROID_HOME/platform-tools
-  printf "\n\nexport ANDROID_HOME=$HOME/Android/Sdk\nexport PATH=\$PATH:\$ANDROID_HOME/tools\nexport PATH=\$PATH:\$ANDROID_HOME/platform-tools" >> $HOME/.bashrc
-  source $HOME/.bashrc
+# if [[ -f "$HOME/Android/Sdk" ]]; then
+#   sudo apt-get install lib32z1 unzip -y 
+#   wget https://dl.google.com/android/repository/sdk-tools-linux-4333796.zip --quiet -P $HOME
+#   unzip $HOME/sdk-tools-linux-4333796.zip -d ./Android/Sdk
+#   rm $HOME/sdk-tools-linux-4333796.zip
 
-  touch $HOME/.android/repositories.cfg
-  yes | ./sdkmanager "platform-tools" "platforms;android-30" "build-tools;30.0.2" 
-  yes | android update sdk --no-ui 
+#   cd $HOME/Android/Sdk/tools/bin
+#   source $HOME/.bashrc
+#   export ANDROID_HOME=$HOME/Android/Sdk
+#   export PATH=$PATH:$ANDROID_HOME/tools
+#   export PATH=$PATH:$ANDROID_HOME/platform-tools
+#   printf "\n\nexport ANDROID_HOME=$HOME/Android/Sdk\nexport PATH=\$PATH:\$ANDROID_HOME/tools\nexport PATH=\$PATH:\$ANDROID_HOME/platform-tools" >> $HOME/.bashrc
+#   source $HOME/.bashrc
+
+#   touch $HOME/.android/repositories.cfg
+#   yes | ./sdkmanager "platform-tools" "platforms;android-30" "build-tools;30.0.2" 
+#   yes | android update sdk --no-ui 
 
 
-  wget https://redirector.gvt1.com/edgedl/android/studio/ide-zips/2021.1.1.21/android-studio-2021.1.1.21-linux.tar.gz -P $HOME --quiet
-  tar -xf $HOME/android-studio-2021.1.1.21-linux.tar.gz -C $HOME
-  rm $HOME/android-studio-2021.1.1.21-linux.tar.gz
+#   wget https://redirector.gvt1.com/edgedl/android/studio/ide-zips/2021.1.1.21/android-studio-2021.1.1.21-linux.tar.gz -P $HOME --quiet
+#   tar -xf $HOME/android-studio-2021.1.1.21-linux.tar.gz -C $HOME
+#   rm $HOME/android-studio-2021.1.1.21-linux.tar.gz
 
-  echo "android" >> $HOME/.installinfo
-else
-  echo "Android/Sdk was not created by some unknown reason"
-fi
+#   echo "android" >> $HOME/.installinfo
+# else
+#   echo "Android/Sdk was not created by some unknown reason"
+# fi
