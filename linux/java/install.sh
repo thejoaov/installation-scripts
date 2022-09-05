f476dea1f3a53198676815d21a98127486888c2a
João Victor <jvictorsantos852@gmail.com>
2022-09-05 09:40:49 -0300

    Update install.sh

--- linux/java/install.sh
+++ linux/java/install.sh
@@ -1,6 +1,6 @@
 if ! java -version 2>&1 >/dev/null | grep -q "java version" ; then
-  sudo apt-get install -qq openjdk-8-jdk openjdk-8-jre -y
-  export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+  sudo apt-get install -qq openjdk-11-jdk openjdk-11-jre -y
+  export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
   export PATH=$PATH:$JAVA_HOME/bin
   printf "\n\nexport JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64\nexport PATH=\$PATH:\$JAVA_HOME/bin" >> $HOME/.bashrc
 fi
