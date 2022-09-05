813e7d318ceaee9bf9507dac3601e928b10e2a3c
João Victor <jvictorsantos852@gmail.com>
2022-09-05 09:55:13 -0300

    Update bootstrap.sh

--- linux/bootstrap.sh
+++ linux/bootstrap.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 # echo | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" &&\
 # echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> ~/.profile &&\
 # eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)" &&\
