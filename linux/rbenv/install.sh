8e036728d2a6d742d1192c84152b9608f51565a6
João Victor <jvictorsantos852@gmail.com>
2022-09-05 09:37:50 -0300

    Update install.sh

--- linux/rbenv/install.sh
+++ linux/rbenv/install.sh
@@ -1,3 +1,11 @@
 export BASE_URL=https://raw.githubusercontent.com/thejoaov/installation-scripts/master
 
+sudo apt install rbenv -y
+rbenv init
+mkdir -p "$(rbenv root)"/plugins
+git clone https://github.com/rbenv/ruby-build.git "$(rbenv root)"/plugins/ruby-build
+rbenv install 2.7.5
+rbenv global 2.7.5
+rbenv rehash
+
 echo "rbenv" >> $HOME/.installinfo
